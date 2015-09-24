fd78144100474f36b908bc0d1127459efa356da0
= <=>
2015-09-24 02:17:51 +0200

    Skeleton for classes

--- src/svd.py
+++ src/svd.py
@@ -5,11 +5,43 @@
 #date            :2015-06-01
 #version         :0.1
 #usage           :python svd.py
-#notes           :
+#notes           :NUMA22
 #python_version  :2.7.9
 #==============================================================================
 
+class Reflection:
+    """A simple example class"""
+    i = 0
+    def __init__(self, ii):
+        self.i = ii
+
+class Householder:
+    j = 0
+    def __init__(self, jj):
+        self.j = jj
+
+class Givens:
+    k = 0
+    def __init__(self, kk):
+        self.k = kk
+
+class SVD:
+    l = 0
+    def __init__(self, ll):
+        self.l = ll
+
 def main():
+        x = Reflection(3.14)
+        print x.i
+        y = Householder(3.15)
+        print y.j
+        z = Givens(3.16)
+        print z.k
+        w = SVD(3.17)
+        print w.l
+        # r = Reflection(v)
+        # a = array([...])
+        # result = r * a
 	print "Singular Value Decomposition"
 
 if __name__ == "__main__":
